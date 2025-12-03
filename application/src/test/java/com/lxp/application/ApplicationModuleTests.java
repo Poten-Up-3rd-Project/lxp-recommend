49d2570a56edfa51e235a23577a2b86ca7bcea96
JaeWoong Eom <zzang6535@gmail.com>
2025-12-03 11:48:55 +0900

    feat: add swagger docs (#10)
    ##  Summary
    - Swagger docs 와 환경 설정
    
    ##  Related Issue
    
    ##  Changes
    - 각 모듈의 resources에 openapi-*.yml 파일로 분리하여 작성하도록 제작
    
    ##  Discussion Topic
    
    ##  Checklist
    - [x] 로컬 테스트 완료
    - [ ] lint 통과(로컬)
    - [ ] 리뷰어 요청(최소 2명), 라벨/스코프 지정
    - [ ] 브랜치 최신화(rebase) 및 충돌 없음
    
    ---------
    
    Co-authored-by: Ahn Nayeong <annay01794@gmail.com>
    Co-authored-by: minyong96 <aldud159357@gmail.com>

--- application/src/test/java/com/lxp/application/ApplicationModuleTests.java
+++ application/src/test/java/com/lxp/application/ApplicationModuleTests.java
@@ -1,6 +1,5 @@
 package com.lxp.application;
 
-import com.lxp.auth.AuthConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
