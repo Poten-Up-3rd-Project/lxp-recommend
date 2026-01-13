4d2eeae1ed6c0edef9a8400ea543d2a97829c8bb
betterb2etr <b2etr@naver.com>
2026-01-13 10:14:47 +0900

    fix: fix endpoints

--- src/main/java/com/lxp/recommend/infrastructure/web/RecommendationController.java
+++ src/main/java/com/lxp/recommend/infrastructure/web/RecommendationController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/recommendations")
+@RequestMapping("/api-v1/recommendations")
 @RequiredArgsConstructor
 public class RecommendationController {
 
